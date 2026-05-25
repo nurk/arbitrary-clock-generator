78c6918fb43241ad02b1f846a3e066f37796514e
Tom Briers <tbriers@telenet.be>
2026-05-25 10:22:20 +0200

    Adjusted calibration

--- src/main.cpp
+++ src/main.cpp
@@ -180,7 +180,7 @@ void initI2CDevices() {
     }
 
     lcd.setCursor(0, 1);
-    if (si5351.init(SI5351_CRYSTAL_LOAD_10PF, 0, -27840)) {
+    if (si5351.init(SI5351_CRYSTAL_LOAD_10PF, 0, -27740)) {
         Serial2.println(F("SI5351 initialized successfully"));
         lcd.print(F("SI5351 initialized"));
     } else {
